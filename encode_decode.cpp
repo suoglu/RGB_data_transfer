a4a3ff50749a29e96277867bc9d1cf0d287ed546
suoglu <yigitsuoglu@live.com>
2016-07-28 14:17:55 +0300

    ASCII encode/decode added

---
+++ encode_decode.cpp
@@ -0,0 +1,53 @@
+//
+//  encode_decode.cpp
+//  testbanch
+//
+//  Created by Yigit Suoglu on 28/07/16.
+//  Copyright © 2016 Yigit Suoglu. All rights reserved.
+//
+
+#include "encode_decode.h"
+
+
+void encodeASCII(char in, bool out[8])
+{
+    for(int j = 0; j < 8; j++)
+        out[j] = 0;
+    
+   // int in = inC;
+    out[0] = 0;
+    if(in % 2)
+        out[7] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[6] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[5] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[4] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[3] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[2] = 1;
+    in = in / 2;
+    if(in % 2)
+        out[1] = 1;
+    in = in / 2;
+}
+
+char decodeASCII(bool in[8])
+{
+    int out = 0;
+    for(int j = 1; j < 8; j++)
+    {
+        out = out * 2;
+        if(in[j])
+            out = out + 1;
+    }
+    return (char) out;
+}
+
---
+++ encode_decode.h
@@ -0,0 +1,20 @@
+//
+//  encode_decode.h
+//  testbanch
+//
+//  Created by Yigit Suoglu on 28/07/16.
+//  Copyright © 2016 Yigit Suoglu. All rights reserved.
+//
+
+#ifndef encode_decode_h
+#define encode_decode_h
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void encodeASCII(char in, bool out[8]);
+char decodeASCII(bool in[8]);
+
+#endif /* encode_decode_h */
