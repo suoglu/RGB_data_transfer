3131db6ef24893245f327d351b14009b49987d94
suoglu <yigitsuoglu@live.com>
2016-07-29 12:42:59 +0300

    Struct for store trashold val

---
+++ colorBit_Struct.h
@@ -0,0 +1,12 @@
+ // Yigit Suoglu Jul 29, 2016
+// structure for tresholds
+
+#ifndef colorBit_Struct_h
+#define colorBit_Struct_h
+
+/*typedef*/ struct colorBit{
+    bool bits[3];
+    uint16_t r_t, g_t, b_t;
+};
+
+#endif /* colorBit_Struct_h */
