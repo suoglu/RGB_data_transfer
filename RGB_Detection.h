2f1dbf79593961ed6867c84b8af81038516232f4
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2016-07-29 15:11:47 +0300

    Typo fix

--- RGB_Detection.h
+++ RGB_Detection.h
@@ -4,7 +4,7 @@
 #ifndef RGB__Detection_h
 #define RGB__Detection_h
 
-#include "colorBit_Struct"
+#include "colorBit_Struct.h"
 
 void detect_rgb(uint16_t r, uint16_t g, uint16_t b, colorBit th_Val[8], bool & r_on, bool & g_on, bool & b_on);
 
