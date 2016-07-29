ef0780a228aa592aa2c6ee29eb9b572815eef109
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2016-07-29 15:12:27 +0300

    Typo fix

--- RGB_Detection.h
+++ RGB_Detection.h
@@ -1,11 +1,11 @@
  // Yigit Suoglu Jul 29, 2016
 // Header for rgb detection code (from given threshold values)
 
-#ifndef RGB__Detection_h
-#define RGB__Detection_h
+#ifndef RGB_Detection_h
+#define RGB_Detection_h
 
 #include "colorBit_Struct.h"
 
 void detect_rgb(uint16_t r, uint16_t g, uint16_t b, colorBit th_Val[8], bool & r_on, bool & g_on, bool & b_on);
 
-#endif /* RGB__Detection_h */
+#endif /* RGB_Detection_h */
