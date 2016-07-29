6b7b4284984db927255e80e00d2af63ae14e78bd
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2016-07-29 12:18:42 +0300

    Update encode_decode.cpp

--- encode_decode.cpp
+++ encode_decode.cpp
@@ -4,18 +4,9 @@
 #include "encode_decode.h"
 
 
-void encodeASCII(char in, bool out0, bool out1, bool out2, bool out3, bool out4, bool out5, bool out6, bool out7)
+void encodeASCII(char in, bool & out0, bool & out1, bool & out2, bool & out3, bool & out4, bool & out5, bool & out6, bool & out7)
 {
    bool out[8];
-   out[0] = out0;
-   out[1] = out1;
-   out[2] = out2;
-   out[3] = out3;
-   out[4] = out4;
-   out[5] = out5;
-   out[6] = out6;
-   out[7] = out7;
-   
     for(int j = 0; j < 8; j++)
         out[j] = 0;
     
@@ -42,10 +33,23 @@ void encodeASCII(char in, bool out0, bool out1, bool out2, bool out3, bool out4,
     if(in % 2)
         out[1] = 1;
     in = in / 2;
+    
+    
+    
+   out0 = out[0];
+   out1 = out[1];
+   out2 = out[2];
+   out3 = out[3];
+   out4 = out[4];
+   out5 = out[5];
+   out6 = out[6];
+   out7 = out[7];
 }
 
-char decodeASCII(bool in[8])
+char decodeASCII(bool in0, bool in1, bool in2, bool in3, bool in4, bool in5, bool in6, bool in7)
 {
+    
+    
     int out = 0;
     for(int j = 1; j < 8; j++)
     {
