fbb58df0d54f47bb4500914dad7f9f794c144810
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2016-07-29 12:10:04 +0300

    Update encode_decode.h
    Now functions take single values instead of array

--- encode_decode.h
+++ encode_decode.h
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void encodeASCII(char in, bool out0, bool out1, bool out2, bool out3, bool out4, bool out5, bool out6, bool out7); //bool out[8]
-char decodeASCII(bool in[8]);
+void encodeASCII(char in, bool & out0, bool & out1, bool & out2, bool & out3, bool & out4, bool & out5, bool & out6, bool & out7); //bool out[8]
+char decodeASCII(bool in0, bool in1, bool in2, bool in3, bool in4, bool in5, bool in6, bool in7);
 
 #endif /* encode_decode_h */
