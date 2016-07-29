68f4150b78242315067cc28a486bd1bca9b1748a
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2016-07-29 11:54:07 +0300

    Update encode_decode.h

--- encode_decode.h
+++ encode_decode.h
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void encodeASCII(char in, bool out[8]);
+void encodeASCII(char in, bool out0, bool out1, bool out2, bool out3, bool out4, bool out5, bool out6, bool out7); //bool out[8]
 char decodeASCII(bool in[8]);
 
 #endif /* encode_decode_h */
