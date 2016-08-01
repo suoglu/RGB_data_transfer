9ede2e05c94e89abb6699609874a707f5f7841d1
calikonur <calik.onur94@gmail.com>
2016-08-01 15:19:01 +0300

    Optimization update

--- ASCII-encode_decode/encode_decode.cpp
+++ ASCII-encode_decode/encode_decode.cpp
@@ -7,46 +7,40 @@
 void encodeASCII(char in, bool & out0, bool & out1, bool & out2, bool & out3, bool & out4, bool & out5, bool & out6, bool & out7)
 {
    out0=0;
-   out0=0;
-   out0=0;
-   out0=0;
-   out0=0;
-   out0=0;
-    
+   out1=0;
+   out2=0;
+   out3=0;
+   out4=0;
+   out5=0;
+   out6=0;
+   out7=0;
+   
    // int in = inC;
-    out[0] = 0;
     if(in % 2)
-        out[7] = 1;
+        out7 = 1;
     in = in / 2;
     if(in % 2)
-        out[6] = 1;
+        out6 = 1;
     in = in / 2;
     if(in % 2)
-        out[5] = 1;
+        out5 = 1;
     in = in / 2;
     if(in % 2)
-        out[4] = 1;
+        out4 = 1;
     in = in / 2;
     if(in % 2)
-        out[3] = 1;
+        out3 = 1;
     in = in / 2;
     if(in % 2)
-        out[2] = 1;
+        out2 = 1;
     in = in / 2;
     if(in % 2)
-        out[1] = 1;
+        out1 = 1;
     in = in / 2;
     
     
     
-   out0 = out[0];
-   out1 = out[1];
-   out2 = out[2];
-   out3 = out[3];
-   out4 = out[4];
-   out5 = out[5];
-   out6 = out[6];
-   out7 = out[7];
+
 }
 
 char decodeASCII(bool in0, bool in1, bool in2, bool in3, bool in4, bool in5, bool in6, bool in7)
